891fde79ac6b2bf740812f3efd4d36de4639c2a5
JasonWongLY <63714795+JasonWongLY@users.noreply.github.com>
2021-11-05 15:45:08 +0800

    Update p2.py

--- Lab3/p2.py
+++ Lab3/p2.py
@@ -1,10 +1,44 @@
+import random
 def quicksort(lista,listb):
-    if len(lista)<=1 or len(listb)<=1:
-        return lista,listb
+    if len(lista)==0 and len(listb)==0:
+        return []
+    lista+=listb
+    p = random.choice(lista)
+    front  = [x for x in lista if x > p]
+    back = [x for x in lista if x <= p]
+
+    if len(front) <= 1 and len(back) <= 1:
+        return front+back
+    elif len(front) <= 1:
+        if(back[0]==back[1]):
+            if (front > back):
+                return front + back
+            else:
+                return back + front
+        else:
+            backp = random.choice(back)
+            back1 = [x for x in back if x > backp]
+            back2 = [x for x in back if x <= backp]
+            return front+quicksort(back1,back2)
+    elif len(back) <= 1:
+        if(front[0]==front[1]):
+            if(front>back):
+                return front+back
+            else:
+                return back+front
+        else:
+            frontp = random.choice(front)
+            front1 = [x for x in front if x > frontp]
+            front2 = [x for x in front if x <= frontp]
+            return quicksort(front1,front2)+back
     else:
-        p = random.choice(lst)
-        low  = [(x for x in lista if x <  p)or(y for y in listb if y <  p)]
-        med  = [(x for x in lista if x ==  p)or(y for y in listb if y==  p)]
-        high = [(x for x in lista if x >  p)or(y for y in listb if y >  p)]
-        return quicksort(low) + med + quicksort(high)
+        frontp = random.choice(front)
+        front1 = [x for x in front if x > frontp]
+        front2 = [x for x in front if x <= frontp]
+        backp = random.choice(back)
+        back1 = [x for x in back if x > backp]
+        back2 = [x for x in back if x <= backp]
+        return quicksort(front1,front2)+[]+quicksort(back1,back2)
+
+print(quicksort([], []))
         
