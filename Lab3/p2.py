2939648c4c62c2afa2df1790f66700ede1c995f2
JasonWongLY <63714795+JasonWongLY@users.noreply.github.com>
2021-11-14 16:19:36 +0800

    Update p2.py

--- Lab3/p2.py
+++ Lab3/p2.py
@@ -1,30 +1,11 @@
 import random
 def quicksort(lista,listb):
-    if len(lista)==0 and len(listb)==0:
-        return []
-    lista+=listb
-    p = random.choice(lista)
-    front  = [x for x in lista if x > p]
-    back = [x for x in lista if x <= p]
-
-    if len(front) <= 1 and len(back) <= 1:
-        return front+back
-    elif len(front) <= 1:
-        if(back[0]==back[1]):
-            if (front > back):
-                return front + back
-            else:
-                return back + front
-        else:
-            return front+quicksort([back[x] for x in range(len(back)) if x < int(len(back)/2)],[back[x] for x in range(len(back)) if x >= int(len(back)/2)])
-    elif len(back) <= 1:
-        if(front[0]==front[1]):
-            if(front>back):
-                return front+back
-            else:
-                return back+front
-        else:
-            return quicksort([front[x] for x in range(len(front)) if x < int(len(front)/2)],[front[x] for x in range(len(front)) if x >= int(len(front)/2)])+back
+    result=lista+listb
+    if len(result)<=1:
+        return result
     else:
-        return quicksort([front[x] for x in range(len(front)) if x < int(len(front)/2)],[front[x] for x in range(len(front)) if x >= int(len(front)/2)])+[]+quicksort([back[x] for x in range(len(back)) if x < int(len(back)/2)],[back[x] for x in range(len(back)) if x >= int(len(back)/2)])
-
+        piv=random.choice(result)
+        result1=[x for x in result if x<piv]
+        result2=[x for x in result if x==piv]
+        result3=[x for x in result if x>piv]
+        return quicksort([],result3)+result2+quicksort(result1,[])
