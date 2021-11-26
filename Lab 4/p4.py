5d8243ab65af363af6973a5bc8bc02edaf45ee5d
JasonWongLY <63714795+JasonWongLY@users.noreply.github.com>
2021-11-26 23:11:35 +0800

    Update p4.py

--- Lab 4/p4.py
+++ Lab 4/p4.py
@@ -1,10 +1,9 @@
-def get_final_grades(filename):
-    students_grades={}
-    with open(filename) as file:
-        for line in file:
-            mark=0
-            result=line.replace('\n','').split(',')
-            for i in range(1,5):
-                mark+=0 if result[i]=='-1' else float(result[i])*(0.2 if i==1 else 0.25 if i==2 or i==3 else 0.3 )
-            students_grades[result[0]]=mark
+import csv
+from functools import reduce
+def get_final_grades():
+    # Weighting for each of the test
+    mark_weight=[0.2,0.25,0.25,0.3]
+    with open('grades.csv') as file:
+        # Using list comprehension with reduce and lambda to sum up the weighted value if the score of the mark is not -1
+        students_grades={line[0]:reduce(lambda x,y:x+y,[float(line[i])*mark_weight[i-1] if float(line[i])!=-1 else 0 for i in range(1,5)])for line in csv.reader(file)}
     return students_grades
